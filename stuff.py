f3124e50fc938eab909b595615653c405ddff952
sam <sampdfont@gmail.com>
2024-05-19 01:04:51 -0400

    added left_game.png stuff.py

---
+++ stuff.py
@@ -0,0 +1,21 @@
+import pyautogui as pyg
+import time
+import os
+
+
+
+#top left  634,706
+#bottom right 1003,749
+regionChatBox = (634,706, 1003, 749)
+
+
+def findClick(nameOfFile):
+      nameOfFile = pyg.locateOnScreen(nameOfFile + '.png', confidence=0.8)
+      pyg.moveTo(nameOfFile.left, nameOfFile.top)
+      pyg.click()
+
+
+
+#pyg.locateOnScreen(nameOfFile + ".png", confidence = 0.5)
+
+#pyg.screenshot(None, region=regionChatBox)
